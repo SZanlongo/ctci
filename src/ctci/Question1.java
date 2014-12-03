a150c26756b78710a6c8ae61e8928cfafadeb531
SZanlongo <SZanlongo@users.noreply.github.com>
2014-12-03 12:44:31 -0500

    Chapter 1 - Question 3

--- src/ctci/Question1.java
+++ src/ctci/Question1.java
@@ -4,6 +4,19 @@ package ctci;
 public class Question1 {
 	// Check if ASCII (128, 256 chars) or Unicode
 
+	public static void main(String[] args) {
+		String[] words = { "abcde", "hello", "world", "java", "poodle" };
+		for (String word : words) {
+			System.out.println(word + ":\t" + isUniqueChars(word));
+		}
+	}
+
+	// Naive approach would compare every char with every other char
+	// Time: O(n^2)
+	// Space: O(1)
+
+	// Time: O(n)
+	// Space: O(1)
 	public static boolean isUniqueChars(String str) {
 		// false if length of string is > unique characters in alphabet
 		if (str.length() > 256) {
@@ -20,11 +33,6 @@ public class Question1 {
 		return true;
 	}
 
-	public static void main(String[] args) {
-		String[] words = { "abcde", "hello", "world", "java", "poodle" };
-		for (String word : words) {
-			System.out.println(word + ":\t" + isUniqueChars(word));
-		}
-	}
-
+	// Could also try sorting the string in O(n log(n)) time
+	// Then compare neighboring chars for match
 }
